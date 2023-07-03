c3e3d538dcec9f9662ab93a2f95aa7373a14976f
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2023-07-03 16:49:03 +0100

    custom import components

--- components/index.ts
+++ components/index.ts
@@ -1,9 +1,12 @@
 import Hero from "./hero/Hero";
 import CustomButton from "./CustomButton";
+import Navbar from "./navigations/Navbar";
+import Footer from "./navigations/Footer"
 
 
 export {
   Hero,
   CustomButton,
- 
+ Navbar,
+ Footer
 }
