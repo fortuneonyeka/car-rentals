7a68f6c8ba4c82e9f91ec115b7316efdd4c84d71
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2023-07-06 15:13:15 +0100

    textstyles and rightarrow types

--- types/index.ts
+++ types/index.ts
@@ -6,6 +6,9 @@ export interface CustomButtonProps {
   containerStyles? : string;
   handleClick?: MouseEventHandler<HTMLButtonElement>;
   btnType?: "button" | "submit";
+  textStyle?: string;
+  rightIcon?: string;
+  isDisabled?: boolean;
 }
 
 
