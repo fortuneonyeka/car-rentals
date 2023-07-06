8ae1805d25d1c2f1a96b318cd40d6ae5c17a6920
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2023-07-06 15:29:41 +0100

    creating car details modal

---
+++ components/Car/CarDetails.tsx
@@ -0,0 +1,17 @@
+import { CarProps } from '@/types';
+import React from 'react'
+
+export interface CarDetailsProps {
+  isOpen: boolean;
+  closeModal: () => void;
+  car: CarProps
+  
+}
+
+const CarDetails = ({isOpen, car, closeModal}:CarDetailsProps) => {
+  return (
+    <div>CarDetails</div>
+  )
+}
+
+export default CarDetails
--- components/index.ts
+++ components/index.ts
@@ -1,20 +1,21 @@
 import Hero from "./hero/Hero";
 import CustomButton from "./CustomButton";
 import Navbar from "./navigations/Navbar";
-import Footer from "./navigations/Footer"
+import Footer from "./navigations/Footer";
 import SearchBar from "./catalogue/SearchBar";
 import CustomFilter from "./catalogue/CustomFilter";
 import SearchManufacturer from "./catalogue/SearchManufacturer";
-import CarCard from "./CarCard";
-
+import CarCard from "./Car/CarCard";
+import CarDetails from "./Car/CarDetails";
 
 export {
   Hero,
   CustomButton,
- Navbar,
- Footer,
- SearchBar,
- CustomFilter,
- SearchManufacturer,
- CarCard
-}
+  Navbar,
+  Footer,
+  SearchBar,
+  CustomFilter,
+  SearchManufacturer,
+  CarCard,
+  CarDetails
+};
