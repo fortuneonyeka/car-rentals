1118634636d231f42bd10cb8826cf1b8d44d6ffc
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2023-07-13 04:30:29 +0100

    update searchparams

--- components/catalogue/SearchBar.tsx
+++ components/catalogue/SearchBar.tsx
@@ -1,9 +1,12 @@
 "use client";
+import React from "react";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 import SearchManufacturer from "./SearchManufacturer";
 import SearchBtnImg from "../../public/magnifying-glass.svg"
 import modelIcon from "../../public/model-icon.png"
 import Image from "next/image";
+import { Router } from "react-router-dom";
 
 const SearchButton = ({otherClasses}:{otherClasses: string}) => (
   <button type="submit" className={`-ml-11 z-10 ${otherClasses}`}>
@@ -15,7 +18,35 @@ const SearchBar = () => {
   const [manufacturer, setManufacturer] = useState('');
   const [model, setModel] = useState('');
 
-  const handleSearch = () => {};
+  const router = useRouter();
+
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (manufacturer === "" && model === "") {
+      return alert("Please fill in the search bar")
+    }
+
+    updateSearchParams(model.toLowerCase(), manufacturer.toLowerCase())
+  };
+
+  const updateSearchParams = (model:string, manufacturer:string) => {
+      const searchParams = new URLSearchParams(window.location.search)
+
+      if (model) {
+        searchParams.set('model', model)
+      }else {
+        searchParams.delete('model')
+      }
+      if (manufacturer) {
+        searchParams.set('manufacturer', manufacturer)
+      }else {
+        searchParams.delete('manufacturer')
+      }
+
+      const newPathname = `${window.location.pathname}?${searchParams.toString()}`
+
+      router.push(newPathname)
+  }
 
   return (
     <form action="" className="searchbar" onSubmit={handleSearch}>
